912d4851751257df6b1a2e1771edacd27091a219
Nagarjuna Yadav K <nagarjunayadavk@gmail.com>
2024-12-19 14:47:33 +0530

    Create faq.vue

---
+++ src/views/faq/faq.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="faq-page">
+    <div>FAQ Page</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Faq",
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+.faq-page {
+  color: orange;
+}
+</style>
