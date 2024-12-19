ebf51caa03ab4ef7e7888a6101d99c651e10c714
Nagarjuna Yadav K <nagarjunayadavk@gmail.com>
2024-12-19 14:42:58 +0530

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <commonHead />
+    <div class="pt-16">
+      <Keep-alive :key="$route.fullPath">
+        <router-view></router-view>
+      </Keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import { commonHead } from "@/components/index";
+
+export default {
+  name: "App",
+  components: {
+    commonHead
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  margin-top: 60px;
+  background-color: #000;
+}
+.pt-16 {
+  padding-top: 7px;
+}
+
+.video-js .vjs-big-play-button {
+  border: none !important;
+  background-color: transparent !important;
+  border-radius: 0px !important;
+}
+</style>
